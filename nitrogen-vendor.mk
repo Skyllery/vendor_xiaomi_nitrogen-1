8cc7dafee4d1908b9eff36e0b8c2b164fe35b6b0
Manu Quijada <emmanuel.quijada@gmail.com>
2019-11-03 09:50:10 +0400

    Update nitrogen-vendor.mk

--- nitrogen-vendor.mk
+++ nitrogen-vendor.mk
@@ -56,7 +56,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/nitrogen/proprietary/framework/dpmapi.jar:system/framework/dpmapi.jar \
     vendor/xiaomi/nitrogen/proprietary/framework/embmslibrary.jar:system/framework/embmslibrary.jar \
     vendor/xiaomi/nitrogen/proprietary/framework/qcrilhook.jar:system/framework/qcrilhook.jar \
-    vendor/xiaomi/nitrogen/proprietary/framework/qti-telephony-common.jar:system/framework/qti-telephony-common.jar \
     vendor/xiaomi/nitrogen/proprietary/framework/tcmclient.jar:system/framework/tcmclient.jar \
     vendor/xiaomi/nitrogen/proprietary/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar:system/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar \
     vendor/xiaomi/nitrogen/proprietary/framework/vendor.qti.data.factory-V1.0-java.jar:system/framework/vendor.qti.data.factory-V1.0-java.jar \
